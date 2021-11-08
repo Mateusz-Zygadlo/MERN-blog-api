808b047c67c95805a3c8bb232248dbfb11ee5c07
Mateusz <mateuszzygadlo00@gmail.com>
2021-11-08 22:12:35 +0100

    Successfull combination of backend to frontend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 require('dotenv').config()
 
@@ -13,6 +15,10 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 const indexRoutes = require('./routes/index');
 
 const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(cors());
+app.use(express.json());
 
 app.use('/', indexRoutes);
 
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    const url = 'http://localhost:8000/posts';
+    const fetchPosts = async () => {
+      const fetchData = await axios.get(url).then((response) => console.log(response.data));
+
+      return fetchData;
+    };
+
+    fetchPosts();
+  }, [])
+
   return(
     <div>
-      <h1>Hello World</h1>
+      {loading ? 
+        <div>Loading</div>
+      :
+        <div>{result}</div>
+      }
     </div>
   )
 }
