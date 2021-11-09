6b5f1fa52a76e4dd855d650b5923162b910b643c
Mateusz <mateuszzygadlo00@gmail.com>
2021-11-10 00:32:21 +0100

    Adding a ready-made registration page

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -15,10 +15,39 @@ exports.allUsers = (req, res, next) => {
 
 exports.newUser = [
   async (req, res, next) => {
-    console.log(req, res);
+    const {email, password, authorPermissions} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    User.find({email: email}).exec((err, result) => {
+      if(err){
+        return next(err);
+      }
+      if(result.length){
+        User.find().exec((err, result) => {
+          if(err){
+            return next(err);
+          }
 
-    return res.json({
-      title: 'work',
+          return res.json({
+            err: 'Account for this email has already been created',
+          })
+        })
+      }else{
+        const user = new User({
+          email,
+          password: hashedPassword,
+          author: authorPermissions,
+          user: true,
+          isAdmin: false,
+        }).save((err) => {
+          if(err){
+            return next(err);
+          }
+        })
+
+        return res.json({
+          title: 'success created account',
+        })
+      }
     })
   }
 ]
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -5,8 +5,10 @@ export const Register = () => {
   const [user, setUser] = useState({
     email: '',
     password: '',
-    passwordTwo: ''
+    passwordTwo: '',
+    authorPermissions: false,
   })
+  const [responseData, setResponseData] = useState(null);
 
   const handleChange = (e) => {
     const {name,value} = e.target
@@ -17,31 +19,52 @@ export const Register = () => {
       })
   }
 
+  const checkboxChange = (e) => {
+    setUser({
+      ...user,
+      'authorPermissions': e.target.checked
+    })
+  }
+
   const createNewUser = async (e) => {
     e.preventDefault();
 
+    setUser({
+      email: '',
+      password: '',
+      passwordTwo: '',
+      authorPermissions: false,
+    })
+    
     const {email, password, passwordTwo} = user;
 
     if(password == passwordTwo && email){
       await axios.post('http://localhost:8000/register', user)
-        .then((res) => console.log(res))
-    }else{
-      console.log('invalid input');
+        .then((res) => setResponseData(res));
     }
   }
 
   return(
-    <div className="p-20">
-      <h1 className="text-5xl py-5">Register form</h1>
-      <form action="http://localhost:8000/register" method="POST">
-        <input name="email" value={user.email} onChange={(e)=>{handleChange(e)}} type="text" placeholder="Enter your email" className="w-64 border-b-2 block mt-4 outline-none focus:border-blue-300 hover:border-gray-300 cursor-pointer transition-colors" />
-        <input name="password" value={user.password} onChange={(e)=>{handleChange(e)}} type="password" placeholder="Enter your password" className="w-64 border-b-2 block mt-2 outline-none focus:border-blue-300 hover:border-gray-300 cursor-pointer transition-colors" />
-        <input name="passwordTwo" value={user.passwordTwo} onChange={(e)=>{handleChange(e)}} type="password" placeholder="Re-enter the password" className="w-64 border-b-2 block mt-2 mb-6 outline-none focus:border-blue-300 hover:border-gray-300 cursor-pointer transition-colors" />
-        <button type="submit" onClick={(e)=>{createNewUser(e)}} className="px-5 py-1 border-2 hover:border-gray-300 transition-colors focus:border-blue-300">Submit</button>
-      </form>
-      <div className="mt-3 w-64">
-        <a href='/login' className="border-b-2 hover:border-gray-300 transition-colors focus:border-blue-300">If you have an account, please click here</a>
+    <>
+      {responseData ? 
+        <div className="flex justify-center">{responseData.data.title || responseData.data.err}</div> 
+      : null }
+      <div className="p-20">
+        <h1 className="text-5xl py-5">Register form</h1>
+        <form action="http://localhost:8000/register" method="POST">
+          <input name="email" value={user.email} onChange={(e)=>{handleChange(e)}} type="text" placeholder="Enter your email" className="w-64 border-b-2 block mt-4 outline-none focus:border-blue-300 hover:border-gray-300 cursor-pointer transition-colors" />
+          <input name="password" value={user.password} onChange={(e)=>{handleChange(e)}} type="password" placeholder="Enter your password" className="w-64 border-b-2 block mt-2 outline-none focus:border-blue-300 hover:border-gray-300 cursor-pointer transition-colors" />
+          <input name="passwordTwo" value={user.passwordTwo} onChange={(e)=>{handleChange(e)}} type="password" placeholder="Re-enter the password" className="w-64 border-b-2 block mt-2 mb-3 outline-none focus:border-blue-300 hover:border-gray-300 cursor-pointer transition-colors" />
+          <div className="flex items-center mb-5">
+            <p className="w-64">do you want to create an account for the author</p>
+            <input type="checkbox" onClick={(e)=>{checkboxChange(e)}}  name="authorPermissions" placeholder="Enter" />
+          </div>
+          <button type="submit" onClick={(e)=>{createNewUser(e)}} className="px-5 py-1 border-2 hover:border-gray-300 transition-colors focus:border-blue-300">Submit</button>
+        </form>
+        <div className="mt-3 w-64">
+          <a href='/login' className="border-b-2 hover:border-gray-300 transition-colors focus:border-blue-300">If you have an account, please click here</a>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
